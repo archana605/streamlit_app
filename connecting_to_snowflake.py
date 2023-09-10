560eb3c34e6ec064df6839a781fc9b252a412b77
archana605 <73113338+archana605@users.noreply.github.com>
2023-09-10 20:04:17 +0530

    Update connecting_to_snowflake.py

--- connecting_to_snowflake.py
+++ connecting_to_snowflake.py
@@ -2,3 +2,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+import os
+
+# Everything is accessible via the st.secrets dict:
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+
+
+# And the root-level secrets are also accessible as environment variables:
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
