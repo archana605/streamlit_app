375ec2c3246118cc22fcffb5e706867b8c3d6d08
archana605 <73113338+archana605@users.noreply.github.com>
2023-09-10 22:05:10 +0530

    Update connecting_to_snowflake.py

--- connecting_to_snowflake.py
+++ connecting_to_snowflake.py
@@ -1,17 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import streamlit as st
-import pandas as pd
-import numpy as np
+import snowflake.connector
 
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(**st.secrets["snowflake"])
 
-import os
+conn = init_connection()
 
-# Everything is accessible via the st.secrets dict:
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
+# Connect to DEMO_DB database
+conn.cursor().execute("USE DATABASE My_database")
 
+# Connect to PETS schema
+conn.cursor().execute("USE SCHEMA My_schema")
 
-# And the root-level secrets are also accessible as environment variables:
-st.write(
-    "Has environment variables been set:",
-    os.environ["db_username"] == st.secrets["db_username"],
-)
+# Perform query.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * FROM employee;")
