1c4b6fe473225b1ed3bf7018d5084e17290b586b
archana605 <73113338+archana605@users.noreply.github.com>
2023-09-10 19:53:52 +0530

    Update connecting_to_snowflake.py

--- connecting_to_snowflake.py
+++ connecting_to_snowflake.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pydeck as pdk
-import scipy
-import altair as alt
-import matplotlib.pyplot as plt
-import time
-from streamlit_extras.switch_page_button import switch_page
-from snowflake.snowpark.context import get_active_session
+
+my_db.connect(username=st.secrets.db_credentials.username, password=st.secrets.db_credentials.password)
