3e0ece0884236fe98c82a7ea51e59e80b844e195
archana605 <73113338+archana605@users.noreply.github.com>
2023-09-15 11:14:22 +0530

    Update connecting_to_snowflake.py

--- connecting_to_snowflake.py
+++ connecting_to_snowflake.py
@@ -10,5 +10,5 @@ st.set_page_config(page_title='Experimental Connection', page_icon=':wave:')
 # Initialize connection.
 session = st.experimental_connection('snowpark').session
 
-temp = session.table('department')
+temp = session.table('v_employee_dept')
 temp
